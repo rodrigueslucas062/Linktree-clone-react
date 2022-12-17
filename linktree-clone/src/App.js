dfee5894a055e75cdff8ac122ff49fafcb7d1634
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-12-17 17:09:29 -0300

    Create Clone

---
+++ linktree-clone/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+import RedesSociais from "./components/Header/RedesSociais/RedesSociais";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <RedesSociais />
+    </>
+  )
+}
+
+export default App;
