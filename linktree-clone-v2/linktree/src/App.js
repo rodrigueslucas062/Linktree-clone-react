55643b307080b2beb4ecc97ac90a6bc83bf90b5f
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-12-31 18:46:42 -0300

    Add a animated background

--- linktree-clone-v2/linktree/src/App.js
+++ linktree-clone-v2/linktree/src/App.js
@@ -1,11 +1,9 @@
-import Header from "./components/Header/Header";
-import RedesSociais from "./components/Header/RedesSociais/RedesSociais";
+//import Header from "./components/Header/Header";
+//import RedesSociais from "./components/Header/RedesSociais/RedesSociais";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <RedesSociais />
     </>
   )
 }
